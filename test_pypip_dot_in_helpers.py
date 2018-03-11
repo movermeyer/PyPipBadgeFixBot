8d8283f12f1cf5c9ce237261dea88b550080cff7
Michael Overmeyer <m.overmeyer@yahoo.ca>
2018-03-11 12:24:28 -0400

    Added some more tests.

--- test_pypip_dot_in_helpers.py
+++ test_pypip_dot_in_helpers.py
@@ -14,6 +14,16 @@ class TestPyPipMigration(unittest.TestCase):
         expected = "https://img.shields.io/pypi/pyversions/fbadmin.svg?style=flat"
         self.assertEqual(pypip_in_to_shields_io(provided), expected)
 
+    def test_with_text(self):
+        provided = "https://pypip.in/py_versions/fbadmin/badge.svg?text=Hello"
+        expected = "https://img.shields.io/pypi/pyversions/fbadmin.svg?label=Hello"
+        self.assertEqual(pypip_in_to_shields_io(provided), expected)
+
+    def test_with_period(self):
+        provided = "https://pypip.in/d/fbadmin/badge.svg?period=day"
+        expected = "https://img.shields.io/pypi/dd/fbadmin.svg"
+        self.assertEqual(pypip_in_to_shields_io(provided), expected)
+
     def test_markdown_links(self):
         provided = "[![Version](https://pypip.in/v/epyper/badge.png)](https://pypi.python.org/pypi/epyper)"
         expected = "[![Version](https://img.shields.io/pypi/v/epyper.svg)](https://pypi.python.org/pypi/epyper)"
